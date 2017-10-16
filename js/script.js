0c305a4cf73af851c913551f7462c0f8b96284af
SHIN_NARITA <triplfjump1417@gmail.com>
2017-10-16 13:36:52 +0900

    boltのCSSをSASS化し、実装しました。

---
+++ js/script.js
@@ -0,0 +1,5 @@
+(function() {
+  document.querySelector(".js-menu-btn").addEventListener("click", function() {
+    document.querySelector(".js-menu").classList.toggle("is-active");
+  }, false);
+})();
